e1d8b431c039b6a1f81ce2333c40efc4066193a1
Kent Pedersen <webzone@kpdesign.dk>
2022-02-22 15:03:08 +0100

    Add project files.

---
+++ Cocktails/CocktailContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace Cocktails
+{
+    class CocktailContext:DbContext
+    {
+
+        public CocktailContext() : base("name=cocktailDBConnectionString")
+        {
+            Database.SetInitializer<CocktailContext>(new CreateDatabaseIfNotExists<CocktailContext>());
+        }
+
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+         
+
+
+    }
+}
---
+++ Cocktails/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocktails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using(CocktailContext context = new CocktailContext())
+            //{
+            //    Ingredient ingredient = new Ingredient() { name = "Vodka" };
+
+            //    context.Ingredients.Add(ingredient);
+            //    context.SaveChanges();
+
+            //}
+
+
+            using (CocktailContext context = new CocktailContext())
+            {
+                var result = from ingredient in context.Ingredients
+                             where ingredient.Name.Contains("vod")
+                             select ingredient;
+
+                foreach (Ingredient drink in result)
+                {
+                    Console.WriteLine(drink.Name);
+                }
+            }
+
+            Console.WriteLine("completed");
+            Console.ReadLine();
+
+
+        }
+    }
+}
